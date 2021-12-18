472b5a779ac86d02915763842a96e9f248753f99
SeanKuehl <teddscottfelt@gmail.com>
2021-12-17 19:31:37 -0500

    continued work on caste management algorithms

--- Scripts/Castes/Caste.gd
+++ Scripts/Castes/Caste.gd
@@ -6,6 +6,10 @@ var casteDesc = ""
 var selectionsList = []
 var rightsList = []
 var occupationList = []
+var amountOfPeopleInCaste = -1
+
+var rightsApproval = -1
+var relativeApproval = 0
 
 var infoList = []
 
@@ -17,8 +21,33 @@ signal UserWantsToEditCaste(casteInfo, casteID)
 func _ready():
 	pass
 
+func CalculateRightsApproval():
+	var total = 0
+
+	for x in range(0,len(rightsList)):
+		var doubleValues = rightsList[x].GetDoubleValues()
+		var indexOfValueToAdd = rightsList[x].GetChosenIndex()
+		total += doubleValues[indexOfValueToAdd]
+
+	rightsApproval = total
+
+func GetRightsApproval():
+	return rightsApproval
+
+func GetRelativeApproval():
+	return relativeApproval
+
+func SetRelativeApproval(newValue):
+	relativeApproval = newValue
+
 func GetID():
 	return ID
+
+func SetAmountOfPeopleInCaste(newValue):
+	amountOfPeopleInCaste = newValue
+
+func GetAmountOfPeopleInCaste():
+	return amountOfPeopleInCaste
 
 func HideMyStuff():
 	$Panel.hide()
@@ -32,7 +61,36 @@ func ShowMyStuff():
 	$NameLabel.show()
 	$EditButton.show()
 
-	mouse_filter = Control.MOUSE_FILTER_STOP
+	mouse_filter = Control.MOUSE_FILTER_STOP	#since it's a control node, unless these two are done then
+	#it will keep picking up input events even if hidden and behind other menus
+
+#occupationName is a string with the name
+func GetOccupationPopPointsAndApproval(occupationName):
+	#get population, points and approval
+	#population is how much of the caste's population is employed in it
+	#points is that population times each of the three multipliers(3 different vals)
+	#approval is the approval of the caste
+
+	var waysToDividePopBy = 1	#start at 1 because economy is an occupation but not listed on file
+	var multipliersList = []
+	var points = [0,0,0]
+	var approval = GetRightsApproval() + GetRelativeApproval()
+	var populationInOccupation = 0
+	var occupationIsEnabled = false
+
+
+	for x in occupationList:
+		if x.GetSelected() == true:
+			waysToDividePopBy += 1
+		if x.GetName() == occupationName and x.GetSelected() == true:
+			occupationIsEnabled = true
+			multipliersList = [x.GetEconomyMultiplier(), x.GetPoliceMultiplier(), x.GetMilitaryMultiplier()]
+
+	if occupationIsEnabled:
+		populationInOccupation = GetAmountOfPeopleInCaste() / waysToDividePopBy
+		points = [multipliersList[0]*populationInOccupation, multipliersList[1]*populationInOccupation, multipliersList[2]*populationInOccupation]
+
+	return [populationInOccupation, approval, points]
 
 func SetNameAndDesc(passedName, passedDesc):
 	casteName = passedName
@@ -42,6 +100,9 @@ func SetNameAndDesc(passedName, passedDesc):
 
 func SetSelections(passedSelections):
 	selectionsList = passedSelections
+
+func GetSelections():
+	return selectionsList
 
 func Init(newCasteInfo):
 
--- Scripts/MainMenu/MainMenu.gd
+++ Scripts/MainMenu/MainMenu.gd
@@ -30,8 +30,11 @@ func Init(econPoints, polPoints, milPoints):
 func AddNewlyCreatedCaste(newCasteInfo):
 	var newCaste = caste.instance()
 	newCaste.Init(newCasteInfo)
+	newCaste.CalculateRightsApproval()
 	newCaste.rect_global_position = Vector2(100,100)
 	newCaste.connect("UserWantsToEditCaste", self, "DoEditCasteMenu")
+	var numOfPeopleInCaste = CalculatePeopleInCasteBeforeConflicts(newCaste.GetSelections())
+	newCaste.SetAmountOfPeopleInCaste(numOfPeopleInCaste)
 	casteList.append(newCaste)
 	add_child(newCaste)
 
@@ -62,6 +65,250 @@ func CalculatePeopleInCasteBeforeConflicts(listOfSelections):
 			populationSizeToAlter -= numberOfPeopleExcluded
 
 	return populationSizeToAlter
+
+func ResolveAmountOfPeopleInCasteConflicts():
+
+
+	for i in range(0,len(casteList)):
+		for k in range(i+1, len(casteList)):
+			ResolveIndividualConflict(casteList[i], casteList[k])
+
+func ResolveIndividualConflict(casteOne, casteTwo):
+	#get their selection lists
+	var firstSelectionList = casteOne.GetSelections()
+	var secondSelectionList = casteTwo.GetSelections()
+
+	var conflict = true
+
+	#for each selection in both caste selection lists:
+	for x in range(0,len(firstSelectionList)):
+		#both lists should be the same size
+
+		#get chosen list
+		var firstChosenList = firstSelectionList[x].GetChosenValues()
+		var secondChosenList = secondSelectionList[x].GetChosenValues()
+
+		for i in range(0, len(firstChosenList)):
+			#both chosen lists should be the same size
+			if firstChosenList[i] == true and secondChosenList[i] == true:
+				conflict = true
+
+
+		if x == (len(firstSelectionList)-1):
+			#it was the last spot, it's really a conflict and must resolved
+
+			if casteOne.GetAmountOfPeopleInCaste() > casteTwo.GetAmountOfPeopleInCaste():
+				var numberOfConflictingSlices = 0
+				var firstPopulation = casteTwo.GetAmountOfPeopleInCaste()	#first as in this algorithm, not the firstCaste necessarily
+				var numberOfSlices = len(secondChosenList)
+				var firstNumOfAllowedSlices = 0
+
+				for k in range(0, len(secondChosenList)):
+					if secondChosenList[k] == true and secondChosenList[k] == firstChosenList[k]:
+						numberOfConflictingSlices += 1
+
+					if secondChosenList[k] == true:
+						firstNumOfAllowedSlices += 1
+
+				var popPerSlice = firstPopulation / firstNumOfAllowedSlices
+				var popToSubtract = popPerSlice * numberOfConflictingSlices
+
+				#set casteOne's new pop, it's old one - popToSubtract. Make sure to update it in casteList! Maybe have another function do that!
+				SetChangeInPopAfterConflict(casteOne.GetID(), popToSubtract)
+			#next if the other one has more, then go to rights approval, then throw exception cause it's a tie!
+
+			elif casteTwo.GetAmountOfPeopleInCaste() > casteOne.GetAmountOfPeopleInCaste():
+				var numberOfConflictingSlices = 0
+				var firstPopulation = casteOne.GetAmountOfPeopleInCaste()	#first as in this algorithm, not the firstCaste necessarily
+				var numberOfSlices = len(firstChosenList)
+				var firstNumOfAllowedSlices = 0
+
+				for k in range(0, len(secondChosenList)):
+					if secondChosenList[k] == true and secondChosenList[k] == firstChosenList[k]:
+						numberOfConflictingSlices += 1
+
+					if secondChosenList[k] == true:
+						firstNumOfAllowedSlices += 1
+
+				var popPerSlice = firstPopulation / firstNumOfAllowedSlices
+				var popToSubtract = popPerSlice * numberOfConflictingSlices
+
+				#set casteOne's new pop, it's old one - popToSubtract. Make sure to update it in casteList! Maybe have another function do that!
+				SetChangeInPopAfterConflict(casteTwo.GetID(), popToSubtract)
+
+			elif casteOne.GetRightsApproval() > casteTwo.GetRightsApproval():
+				var numberOfConflictingSlices = 0
+				var firstPopulation = casteTwo.GetAmountOfPeopleInCaste()	#first as in this algorithm, not the firstCaste necessarily
+				var numberOfSlices = len(secondChosenList)
+				var firstNumOfAllowedSlices = 0
+
+				for k in range(0, len(secondChosenList)):
+					if secondChosenList[k] == true and secondChosenList[k] == firstChosenList[k]:
+						numberOfConflictingSlices += 1
+
+					if secondChosenList[k] == true:
+						firstNumOfAllowedSlices += 1
+
+				var popPerSlice = firstPopulation / firstNumOfAllowedSlices
+				var popToSubtract = popPerSlice * numberOfConflictingSlices
+
+				#set casteOne's new pop, it's old one - popToSubtract. Make sure to update it in casteList! Maybe have another function do that!
+				SetChangeInPopAfterConflict(casteOne.GetID(), popToSubtract)
+
+			elif casteTwo.GetRightsApproval() > casteOne.GetRightsApproval():
+				var numberOfConflictingSlices = 0
+				var firstPopulation = casteOne.GetAmountOfPeopleInCaste()	#first as in this algorithm, not the firstCaste necessarily
+				var numberOfSlices = len(firstChosenList)
+				var firstNumOfAllowedSlices = 0
+
+				for k in range(0, len(secondChosenList)):
+					if secondChosenList[k] == true and secondChosenList[k] == firstChosenList[k]:
+						numberOfConflictingSlices += 1
+
+					if secondChosenList[k] == true:
+						firstNumOfAllowedSlices += 1
+
+				var popPerSlice = firstPopulation / firstNumOfAllowedSlices
+				var popToSubtract = popPerSlice * numberOfConflictingSlices
+
+				#set casteOne's new pop, it's old one - popToSubtract. Make sure to update it in casteList! Maybe have another function do that!
+				SetChangeInPopAfterConflict(casteTwo.GetID(), popToSubtract)
+
+			#otherwise it's a tie, this should never happen so for now just throw exception
+			else:
+				assert(1 != 0, "Two castes have the same pop size and rights approval. What? How?")
+
+		else:
+			#it's not a conflict, don't need to change anything
+			pass
+
+#call this function when a new caste is added or an old one is changed
+func ResetCastesAfterChange():
+	for x in range(0,len(casteList)):
+		var peopleInCaste = CalculatePeopleInCasteBeforeConflicts(casteList[x].GetSelections())
+		casteList[x].SetAmountOfPeopleInCaste(peopleInCaste)
+
+	ResolveAmountOfPeopleInCasteConflicts()
+
+	CalculateCasteRelativeApproval()
+
+	#need get military etc. points and approval
+
+func GetOccupationPopPointsAndApproval():
+	#at end determine number in "economy" role by subtracting other role pops from
+	#population size
+	var militaryPopList = []
+	var militaryApprovalList = []
+
+	var policePopList = []
+	var policeApprovalList = []
+
+	var overallPointsList = [0,0,0]
+
+	for caste in casteList:
+		# getocpoppointsandapproval returns [populationInOccupation, approval, points] where points is a list in itself
+		var values = caste.GetOccupationPopPointsAndApproval("Military")
+		militaryPopList.append(values[0])
+		militaryApprovalList.append(values[1])
+		var pointsList = values[2]
+		overallPointsList[0] += pointsList[0]
+		overallPointsList[1] += pointsList[1]
+		overallPointsList[2] += pointsList[2]
+
+		values = caste.GetOccupationPopPointsAndApproval("Police")
+		policePopList.append(values[0])
+		policeApprovalList.append(values[1])
+		pointsList = values[2]
+		overallPointsList[0] += pointsList[0]
+		overallPointsList[1] += pointsList[1]
+		overallPointsList[2] += pointsList[2]
+
+
+	#find wieghted approvals(weighted to population)
+	#polAveragedApproval = polWeightedApprovalSum / polTotalPop;
+	var totalPop = 0
+	var wieghtedApproval = 0
+
+	var milAverageApproval = 0
+	var polAverageApproval = 0
+	#will need total pop for each occupation! At least for end for figuring out
+	#how many in economy role etc.
+	for x in range(0, len(militaryPopList)):
+		#the pop and approval lists should be the same length
+		#unlike the mil and pol lists of things
+		totalPop += militaryPopList[x]
+		wieghtedApproval += militaryPopList[x] * militaryApprovalList[x]	#do population times approval
+	milAverageApproval = wieghtedApproval / totalPop
+
+	totalPop = 0
+	wieghtedApproval = 0
+
+	for x in range(0, len(policePopList)):
+		totalPop += policePopList[x]
+		wieghtedApproval += policePopList[x] * policeApprovalList[x]
+	polAverageApproval = wieghtedApproval / totalPop
+
+
+func CalculateCasteRelativeApproval():
+	for i in range(0,len(casteList)):
+		for k in range(0,len(casteList)):
+			#don't compare the caste to itself
+			if i == k:
+				pass
+			else:
+				#call relative approval function
+				SetRelativeApprovalBetweenTwoCastes(casteList[i], casteList[k])
+
+
+func SetRelativeApprovalBetweenTwoCastes(casteOne, casteTwo):
+	var firstPop = casteOne.GetAmountOfPeopleInCaste()
+	var secondPop = casteTwo.GetAmountOfPeopleInCaste()
+
+	var firstRightsApproval = casteOne.GetRightsApproval()
+	var secondRightsApproval = casteTwo.GetRightsApproval()
+
+	var rightsPoints = 0
+	var popPoints = (abs(firstPop - secondPop)) / 100
+
+	if firstRightsApproval > secondRightsApproval:
+		rightsPoints = (abs(firstRightsApproval - secondRightsApproval)) / 10
+
+			#this is the relative approval from only this other caste, there could be more so it's added to the current points, not set
+		var newRelativeApproval = casteOne.GetRelativeApproval() + (rightsPoints * popPoints)
+
+			#only first needs to be set because the other caste will have it's time when it's the one being compared
+
+		SetCasteRelativeApproval(casteOne.GetID(), newRelativeApproval)
+
+	elif secondRightsApproval > firstRightsApproval:
+		rightsPoints = (abs(secondRightsApproval - firstRightsApproval)) / 10
+
+			#this is the relative approval from only this other caste, there could be more so it's added to the current points, not set
+		var newRelativeApproval = casteOne.GetRelativeApproval() + (rightsPoints * popPoints)
+
+			#only first needs to be set because the other caste will have it's time when it's the one being compared
+			#if second is treated better, then relative approval is negative
+
+		SetCasteRelativeApproval(casteOne.GetID(), newRelativeApproval*-1)
+
+	else:
+		#they are the same and there's no relative approval to be gained or lost
+		pass
+
+
+func SetCasteRelativeApproval(casteID, newRelativeApproval):
+	for x in range(0,len(casteList)):
+		if casteList[x].GetID() == casteID:
+
+			casteList[x].SetRelativeApproval(newRelativeApproval)
+
+
+func SetChangeInPopAfterConflict(casteID, popToSubtract):
+	for x in range(0,len(casteList)):
+		if casteList[x].GetID() == casteID:
+			var newAmountOfPeople = casteList[x].GetAmountOfPeopleInCaste() - popToSubtract
+			casteList[x].SetAmountOfPeopleInCaste(newAmountOfPeople)
+
 
 
 func ConnectSignalsFromRoot(root):
